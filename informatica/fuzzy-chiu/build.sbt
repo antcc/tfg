460ddfc16d5b5b76913ad400330b3f178d781dae
antcc <antoniocoin07@gmail.com>
2020-02-03 14:12:38 +0100

    Fuzzy C Means

--- informatica/fuzzy-chiu/build.sbt
+++ informatica/fuzzy-chiu/build.sbt
@@ -3,5 +3,6 @@ version := "1.0"
 scalaVersion := "2.11.12"
 
 libraryDependencies ++= Seq(
-	"org.apache.spark" %% "spark-core" % "2.4.4" % "provided"
+	"org.apache.spark" %% "spark-core" % "2.4.4" % "provided",
+        "org.apache.spark" %% "spark-mllib" % "2.4.4" % "provided"
 )
--- informatica/fuzzy-chiu/src/main/scala/fuzzy-chiu.scala
+++ informatica/fuzzy-chiu/src/main/scala/fuzzy-chiu.scala
@@ -4,13 +4,16 @@ Antonio Coín Castro.
 */
 
 import scala.io.Source
+import scala.math.{pow, abs, sqrt}
 import java.io.File
 import java.io.PrintWriter
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkContext, SparkConf}
+import org.apache.spark.rdd.RDD
 import org.apache.spark.HashPartitioner
+import org.apache.spark.mllib.linalg.{Vector, Vectors}
+import breeze.linalg.DenseVector
 
-object SparkFuzzyChiu {
+object FuzzyChiu {
 
   /********** Constants *************/
 
@@ -21,25 +24,28 @@ object SparkFuzzyChiu {
   val lower_bound = 0.15
   val upper_bound = 0.5
 
-  /********** Variables *************/
+  val m = 2
+  val eps = 1e-6
+  val max_iter = 200
 
-  var saveFile = true
-  var fs_hdfs = false
+  val saveFile = true
+  val fs_hdfs = false
+  val local = false
 
-  /********* Functions **********/
+  /********* Helper Functions **********/
 
   def writeToFile(p: String, s: String): Unit = {
     val pw = new PrintWriter(new File(p))
     try pw.write(s) finally pw.close()
   }
 
-  def resultToString(centers : List[List[Double]]) = {
+  def resultToString(centers : List[Vector]) = {
     var result = ""
     for (center <- centers) {
-      for (i <- 0 to center.length - 1) {
+      for (i <- 0 to center.size - 1) {
         result += center(i)
 
-        if (i != center.length - 1)
+        if (i != center.size - 1)
           result += ","
         else
           result += "\n"
@@ -48,80 +54,23 @@ object SparkFuzzyChiu {
     result
   }
 
-  def distanceSquared(xs: List[Double], ys: List[Double]) = {
-    (xs zip ys).map{ case (x,y) => math.pow(y - x, 2) }.sum
-  }
+  /********* Chiu Variants **********/
 
-  def main(args: Array[String]) = {
-
-    // Configure spark
-    val conf = new SparkConf().setAppName("FuzzyChiu")
-    val sc = new SparkContext(conf)
-
-    // Read file
-    var inputFile = args.headOption.getOrElse {
-      Console.err.println("error: se necesita un fichero de entrada\n")
-      sys.exit(1)
-    }
-
-    if (fs_hdfs)
-      inputFile = "file://" + inputFile
-
-    // Load input file into RDD
-    val input = sc.textFile(inputFile)
-    val points = input.map(line => line.split(',').map(_.toDouble).toList)
-    val indexed = points.zipWithIndex()
-    val indexedPoints = indexed.map{case (k, v) => (v, k)}
-
+  def globalChiu(points : RDD[Vector]): List[Vector] = {
     val numPoints = points.count()
-    var centers = List[List[Double]]()
-
-    println("----> LEÍDOS " + points.count + " PUNTOS\n")
-
-    println("----> PAREJAS HECHAS\n")
-
+    var centers = List[Vector]()
     val pairs = points.cartesian(points)
 
-    val indices = sc.parallelize(
-                    for(i <- 0L until numPoints; j <- 0L until numPoints; if i > j) yield (i, j))
-    val joined = indices.join(indexedPoints).map {case (i, (j, v)) => (j, (i, v))}
-    val d1 = joined.join(indexedPoints).map {case (j, ((i, v1), v2)) => (i, (v1, v2))}
-    val d2 = joined.join(indexedPoints).map {case (j, ((i, v1), v2)) => (j, (v1, v2))}
-    val dist1 = d1.mapValues{case (v1, v2) => math.exp(-alpha * distanceSquared(v1, v2))}.
-                reduceByKey(_ + _)
-    val dist2 = d2.mapValues{case (v1, v2) => math.exp(-alpha * distanceSquared(v1, v2))}.
-                            reduceByKey(_ + _)
-    val pot = dist1.join(dist2).mapValues{x => x.productIterator.toList.map{y => y.asInstanceOf[Double]}.sum + 1}
-
-
-    println("-----> COSAS\n")
-
-    var b = dist1.collect()
-    for(p <- b) {
-      println(p)
-    }
-    var c = dist2.collect()
-    for(p <- b) {
-      println(p)
-    }
-
     // Compute initial potential
-    var potential = pairs.map{ case (a,b) => (a,
-                    math.exp(-alpha * distanceSquared(a, b)))}
+    var potential = pairs.map{case (a,b) => (a,
+                    math.exp(-alpha * Vectors.sqdist(a, b)))}
                     .partitionBy(new HashPartitioner(101))
                     .reduceByKey(_ + _)
                     .cache()
 
-    /**println("POTENCIALES INICIALES:")
-    var h = potential.collect()
-    for (i <- h)
-      println(i._2)*/
-
     var chosenTuple = potential.max()(Ordering[Double].on(x => x._2))
     var chosenCenter = chosenTuple._1
     var firstCenterPotential, chosenPotential = chosenTuple._2
-
-    println("\nPOTENCIAL ELEGIDO: " + chosenPotential)
 
     // First center
     centers = chosenCenter :: centers
@@ -131,14 +80,13 @@ object SparkFuzzyChiu {
     while (!stop) {
       // Revise potential of points
       potential = potential.map{case (a,b) => (a,
-                    b - chosenPotential * math.exp(-beta * distanceSquared(a, chosenCenter)))}.cache()
+                    b - chosenPotential * math.exp(-beta * Vectors.sqdist(a, chosenCenter)))}.cache()
 
       // Find new center
       chosenTuple = potential.max()(Ordering[Double].on(x => x._2))
       chosenCenter = chosenTuple._1
       chosenPotential = chosenTuple._2
       test = true
-      println("POTENCIAL ELEGIDO: " + chosenPotential)
 
       // Check stopping condition
       while (test) {
@@ -158,7 +106,7 @@ object SparkFuzzyChiu {
 
         // Gray zone
         else {
-          var dmin = centers.map {x => math.sqrt(distanceSquared(chosenCenter, x)) }
+          var dmin = centers.map{x => sqrt(Vectors.sqdist(chosenCenter, x))}
                      .reduceLeft(_ min _)
 
           // Accept and continue
@@ -183,7 +131,260 @@ object SparkFuzzyChiu {
       }
     }
 
-    println("\n----> NÚMERO DE CENTROS ENCONTRADOS: " + centers.length)
+    return centers
+  }
+
+  def localChiu(it: Iterator[Vector]): Iterator[Vector] = {
+    var centers = List[Vector]()
+    var potential = List[(Vector, Double)]()
+    var numPoints = 0
+    var (it1, it2) = it.duplicate
+
+    while (it1.hasNext) {
+      var dup = it2.duplicate
+      it2 = dup._1
+      var it3 = dup._2
+      var cur = it1.next
+      var dist = 0.0
+
+      while(it3.hasNext) {
+        var aux : Vector = it3.next
+        dist = dist + math.exp(-alpha * Vectors.sqdist(cur, aux))
+      }
+
+      potential = (cur, dist) :: potential
+      numPoints = numPoints + 1
+    }
+
+    println("\n--------------> PARTICIÓN CON " + numPoints + " PUNTOS\n")
+
+    var chosenTuple = potential maxBy { _._2 }
+    var chosenCenter = chosenTuple._1
+    var firstCenterPotential, chosenPotential = chosenTuple._2
+
+    //println("\n--------------> POTENCIAL ELEGIDO: " + chosenPotential)
+
+    // First center
+    centers = chosenCenter :: centers
+
+    var stop = false
+    var test = true
+    while (!stop) {
+      // Revise potential of points
+      potential = potential.map{case (a,b) => (a,
+                    b - chosenPotential *
+                    math.exp(-beta * Vectors.sqdist(a, chosenCenter)))}
+
+      // Find new center
+      chosenTuple = potential maxBy { _._2 }
+      chosenCenter = chosenTuple._1
+      chosenPotential = chosenTuple._2
+      test = true
+
+      //println("\n--------------> POTENCIAL ELEGIDO: " + chosenPotential)
+
+      // Check stopping condition
+      while (test) {
+        // Accept and continue
+        if (chosenPotential > upper_bound * firstCenterPotential) {
+          centers = chosenCenter :: centers
+          test = false
+          if (centers.length >= numPoints)
+            stop = true
+        }
+
+        // Reject and stop
+        else if (chosenPotential < lower_bound * firstCenterPotential) {
+          test = false
+          stop = true
+        }
+
+        // Gray zone
+        else {
+          var dmin = centers.map{x => sqrt(Vectors.sqdist(chosenCenter, x))}
+                     .reduceLeft(_ min _)
+
+          // Accept and continue
+          if ((dmin / ra) + (chosenPotential / firstCenterPotential) >= 1) {
+            centers = chosenCenter :: centers
+            test = false
+            if (centers.length >= numPoints)
+              stop = true
+          }
+
+          // Reject and re-test
+          else {
+            potential = potential.map{case (a,b) => (a,
+                              {if (a == chosenCenter) 0.0 else b})}
+
+            // Find new center
+            chosenTuple = potential maxBy { _._2 }
+            chosenCenter = chosenTuple._1
+            chosenPotential = chosenTuple._2
+
+            //println("\n--------------> POTENCIAL ELEGIDO: " + chosenPotential)
+          }
+        }
+      }
+    }
+
+    println("\n--------------> PARTICIÓN CON " + centers.length + " CENTROS\n")
+
+    centers.iterator
+  }
+
+  /** FUZZY C MEANS **/
+
+  def add(x: Vector, y: Vector) = {
+    var bx = new DenseVector(x.toArray)
+    var by = new DenseVector(y.toArray)
+    Vectors.dense((bx + by).toArray)
+  }
+
+  def substract(x: Vector, y: Vector) = {
+    var bx = new DenseVector(x.toArray)
+    var by = new DenseVector(y.toArray)
+    Vectors.dense((bx - by).toArray)
+  }
+
+  def multiply(x: Vector, l: Double) = {
+    var bx = new DenseVector(x.toArray)
+    Vectors.dense((l * bx).toArray)
+  }
+
+  def vabs(x: Vector) = {
+    Vectors.dense(x.toArray.map(l => abs(l)))
+  }
+
+  def computeMatrix(x: Vector, c: Array[Vector]) = {
+    var numCluster = c.length
+    var membership = Array.ofDim[Double](numCluster)
+    var distances = Array.ofDim[Double](numCluster)
+
+    for (j <- 0 to numCluster - 1) {
+      val denom = c.map {ck =>
+        pow(sqrt(Vectors.sqdist(x, c(j))) / sqrt(Vectors.sqdist(x, ck)), 2.0 / (m - 1))
+      }.sum
+
+      if (denom.isInfinity)
+        membership(j) = 0.0
+      else if (denom.isNaN)
+        membership(j) = 1.0
+      else
+        membership(j) = 1.0 / denom
+    }
+
+    (x, Vectors.dense(membership))
+  }
+
+  def stopping_cond(old: RDD[(Vector, Vector)], curr: RDD[(Vector, Vector)]): Boolean = {
+    var diff = old.join(curr)
+        .map{case(x, (u, v)) =>
+          {
+            var t = vabs(substract(v, u))
+            t(t.argmax)
+          }
+        }
+        .max()
+
+    diff < eps
+  }
+
+  // Return loss function
+  def fuzzyCMeans(points : RDD[Vector], init_centers : List[Vector]): (List[Vector], RDD[(Vector, Vector)], Double, Double, Int) = {
+    val numCluster = init_centers.length
+
+    // Compute initial matrix
+    var centers = init_centers.toArray
+    var new_matrix: RDD[(Vector, Vector)] = points.map{x => computeMatrix(x, centers)}
+                                                  .cache()
+    var old_matrix: RDD[(Vector, Vector)] = null
+    var iter: Int = 0
+
+    var init_loss = new_matrix.map {case (x, r) =>
+      {
+        var temp = 0.0
+        for (j <- 0 to r.size - 1)
+          temp = temp + pow(r(j), m) * Vectors.sqdist(x, centers(j))
+        temp
+      }
+    }.sum()
+
+    do {
+      old_matrix = new_matrix
+
+      // Update centers
+      for (j <- 0 to numCluster - 1) {
+        var temp = old_matrix.map{
+          case (x, r) => (multiply(x, pow(r(j), m)), pow(r(j), m))
+        }
+        .reduce{(x, y) => (add(x._1, y._1), x._2 + y._2)}
+
+        centers(j) = multiply(temp._1, 1.0 / temp._2)
+      }
+
+      // Update membership matrix
+      new_matrix = old_matrix.map{case (x, r) => computeMatrix(x, centers)}
+                             .cache()
+      iter = iter + 1
+
+      println("\n-------> ITERACIÓN " + iter)
+
+    } while (!stopping_cond(old_matrix, new_matrix) && iter < max_iter)
+
+    var loss = new_matrix.map {case (x, r) =>
+      {
+        var temp = 0.0
+        for (j <- 0 to r.size - 1)
+          temp = temp + pow(r(j), m) * Vectors.sqdist(x, centers(j))
+        temp
+      }
+    }.sum()
+
+    (centers.toList, new_matrix, init_loss, loss, iter)
+  }
+
+  def main(args: Array[String]) = {
+    // Configure spark
+    val conf = new SparkConf().setAppName("FuzzyChiu")
+    val sc = new SparkContext(conf)
+
+    // Read file
+    var inputFile = args.headOption.getOrElse {
+      Console.err.println("error: se necesita un fichero de entrada\n")
+      sys.exit(1)
+    }
+
+    if (fs_hdfs)
+      inputFile = "file://" + inputFile
+
+    // Load input file into RDD
+    val input = sc.textFile(inputFile)
+    val points = input.map(line => Vectors.dense(line.split(',').map(_.toDouble)))
+                 .repartition(4)
+                 .cache()
+
+    println("\n--------------> LEÍDOS " + points.count + " PUNTOS\n")
+
+    // Compute centroids
+    var centers = List[Vector]()
+    if (local) {
+      centers = points.mapPartitions(localChiu).collect().toList
+    }
+    else {
+      centers = globalChiu(points)
+    }
+
+    val fcm = fuzzyCMeans(points, centers)
+
+    println("\n--------------> NÚMERO DE CENTROS ENCONTRADOS: " + centers.length)
+    println("--------------> FUNCIÓN DE PÉRDIDA: " + fcm._3)
+    println("--------------> CENTROS ENCONTRADOS:\n" + resultToString(centers))
+
+    println("\n--------------> NÚMERO DE CENTROS ACTUALIZADOS: " + fcm._1.length)
+    println("--------------> FUNCIÓN DE PÉRDIDA: " + fcm._4)
+    println("--------------> ITERACIONES: " + fcm._5)
+    println("--------------> CENTROS ACTUALIZADOS:\n" + resultToString(fcm._1))
 
     if (saveFile) {
       writeToFile("output/out_denorm.txt", resultToString(centers))
